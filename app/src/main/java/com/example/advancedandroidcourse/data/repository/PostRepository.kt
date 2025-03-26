20bb1a2d8a8cc1bbf66c99be18787c346c564ac3
XuanzhaoS <t3zhxu00@students.oamk.fi>
2025-03-26 14:20:33 +0200

    Tips display on HomeScreen

--- app/src/main/java/com/example/advancedandroidcourse/data/repository/PostRepository.kt
+++ app/src/main/java/com/example/advancedandroidcourse/data/repository/PostRepository.kt
@@ -1,7 +1,12 @@
 package com.example.advancedandroidcourse.data.repository
 
+import android.util.Log
+import com.example.advancedandroidcourse.data.model.Post
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -28,4 +33,21 @@ class PostRepository @Inject constructor(
             .addOnSuccessListener { onComplete(true) }
             .addOnFailureListener{ onComplete(false) }
     }
+
+//    Fetching Data
+    fun getPosts(): Flow<List<Post>> = flow {
+        try {
+            val querySnapshot = firestore.collection("tips").get().await()
+            Log.d("PostRepository", "Documents size: ${querySnapshot.documents.size}")
+            val posts = querySnapshot.documents.mapNotNull { documentSnapshot ->
+                documentSnapshot.toObject(Post::class.java)
+            }
+            emit(posts)
+        } catch (e: Exception) {
+            Log.e("PostRepository", "Error fetching posts: ${e.message}")
+            emit(emptyList<Post>())
+        }
+}
+
+
 }
--- app/src/main/java/com/example/advancedandroidcourse/presentation/main/PostViewModel.kt
+++ app/src/main/java/com/example/advancedandroidcourse/presentation/main/PostViewModel.kt
@@ -1,7 +1,11 @@
 package com.example.advancedandroidcourse.presentation.main
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.advancedandroidcourse.data.model.Post
 import com.example.advancedandroidcourse.data.repository.PostRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -12,10 +16,25 @@ class PostViewModel @Inject constructor(
     private val postRepository: PostRepository
 ) : ViewModel() {
 
+//    Save posts fetching from firestore
+    private val _posts = MutableLiveData<List<Post>>()
+    val posts: LiveData<List<Post>> get() = _posts
+
     fun createPost(title: String, content: String, imageUrl: String?, onComplete: (Boolean) -> Unit) {
 
         viewModelScope.launch {
             postRepository.addPost(title, content, imageUrl, onComplete)
+        }
+    }
+
+//    Fetching data
+    fun getPosts() {
+        viewModelScope.launch {
+            postRepository.getPosts()
+                .collect { postList ->
+                    Log.d("PostViewModel", "Fetched posts: $postList")
+                    _posts.value = postList
+                }
         }
     }
 }
