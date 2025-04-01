05d8e195a6dc73162f731e7e4d9c631858b41bda
XuanzhaoS <t3zhxu00@students.oamk.fi>
2025-04-01 14:19:32 +0300

    Display postImage in homeScreen

--- app/src/main/java/com/example/advancedandroidcourse/presentation/composables/PostItem.kt
+++ app/src/main/java/com/example/advancedandroidcourse/presentation/composables/PostItem.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,9 +30,19 @@ fun PostItem(postDetails: PostDetails) {
         modifier = Modifier.padding(8.dp)
     ) {
         Column {
+            Image(
+                painter = rememberImagePainter(postDetails.post.images[0]),
+                contentDescription = "Post Image",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            )
             Text(text = postDetails.post.title)
             Text(text = postDetails.post.content)
         }
+
+//  Display author's information
         Row (
             verticalAlignment = Alignment.CenterVertically
         ) {
