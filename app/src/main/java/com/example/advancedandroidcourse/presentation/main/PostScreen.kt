f46d9d62b609fb088f6f4a69d44f95b4678bdd5e
shyamharsh <t3wika00@students.oamk.fi>
2025-04-02 20:31:32 +0300

    UI enhancement of Postscreen

--- app/src/main/java/com/example/advancedandroidcourse/presentation/main/PostScreen.kt
+++ app/src/main/java/com/example/advancedandroidcourse/presentation/main/PostScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -41,6 +42,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.TextField
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -67,7 +69,13 @@ fun PostScreen(
     }
 
     //Tag selection state
-    val tagOptions = listOf("essentials", "transportation", "official matters", "housing & jobs", "language & integration")
+    val tagOptions = listOf(
+        "essentials",
+        "transportation",
+        "official matters",
+        "housing & jobs",
+        "language & integration"
+    )
     var selectedTag by remember { mutableStateOf("") }
     var expanded by remember { mutableStateOf(false) }
 
@@ -78,34 +86,42 @@ fun PostScreen(
     ) {
         //Back button
         IconButton(onClick = onBackClick) {
-            Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = stringResource(id = R.string.back))
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = stringResource(id = R.string.back)
+            )
         }
 
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(12.dp))
 
         //Image selection-using placeholder and coil for loading image
-        Row(verticalAlignment = Alignment.CenterVertically) {
+        Row(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
             imageUris.take(3).forEach { uri ->
-            AsyncImage(
-                model = uri,
-                contentDescription = stringResource(id = R.string.add),
-                modifier = Modifier
-                    .size(80.dp)
-                    .background(Color.Gray, RoundedCornerShape(8.dp))
+                AsyncImage(
+                    model = uri,
+                    contentDescription = stringResource(id = R.string.add),
+                    modifier = Modifier
+                        .size(100.dp)
+                        .background(Color.LightGray, RoundedCornerShape(12.dp))
                     //.clickable {imagePickerLauncher.launch("image/*") } //open system file picker
-            )
+                )
 
-            Spacer(modifier = Modifier.width(12.dp))
+                //Spacer(modifier = Modifier.width(12.dp))
             }
             //Image picker button
             Box(
                 modifier = Modifier
-                    .size(80.dp)
-                    .background(Color.LightGray, RoundedCornerShape(8.dp))
+                    .size(100.dp)
+                    .background(Color.LightGray, RoundedCornerShape(12.dp))
                     .clickable { imagePickerLauncher.launch("image/*") },
                 contentAlignment = Alignment.Center
             ) {
-                Text("+", fontSize = 24.sp, color = Color.DarkGray)
+                //Text("+", fontSize = 24.sp, color = Color.DarkGray)
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.add),
+                    tint = Color.DarkGray
+                )
             }
         }
 
@@ -113,37 +129,38 @@ fun PostScreen(
             Text("Please upload at least one image", color = Color.Red, fontSize = 14.sp)
         }
 
-        Spacer(modifier = Modifier.height(12.dp))
+        Spacer(modifier = Modifier.height(16.dp))
 
         //Title input
-        TextField(
+        OutlinedTextField(
             value = title,
             onValueChange = { title = it },
+            label = { Text(stringResource(R.string.write_your_title_here)) },
             textStyle = TextStyle(fontSize = 18.sp),
-            placeholder = { Text(stringResource(id = R.string.write_your_title_here), color = Color.Gray) },
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color(0xFFF0F0F0), RoundedCornerShape(8.dp))
-                .padding(12.dp)
+            //placeholder = { Text(stringResource(id = R.string.write_your_title_here), color = Color.Gray) },
+            modifier = Modifier.fillMaxWidth()
+            //.background(Color(0xFFF0F0F0), RoundedCornerShape(8.dp))
+            //.padding(12.dp)
         )
 
         if (showError && title.isBlank()) {
             Text("Title cannot be empty", color = Color.Red, fontSize = 14.sp)
         }
 
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(12.dp))
 
         //Content input
-        TextField(
+        OutlinedTextField(
             value = content,
             onValueChange = { content = it },
+            label = { Text(stringResource(R.string.whats_your_tips)) },
             textStyle = TextStyle(fontSize = 16.sp),
-            placeholder = { Text(stringResource(id = R.string.whats_your_tips), color = Color.Gray) },
+            //placeholder = { Text(stringResource(id = R.string.whats_your_tips), color = Color.Gray) },
             modifier = Modifier
                 .fillMaxWidth()
-                .height(120.dp)
-                .background(Color(0xFFF0F0F0), RoundedCornerShape(8.dp))
-                .padding(12.dp)
+                .height(150.dp)
+            //.background(Color(0xFFF0F0F0), RoundedCornerShape(8.dp))
+            //.padding(12.dp)
         )
 
         if (showError && content.isBlank()) {
@@ -155,19 +172,20 @@ fun PostScreen(
         //Dropdown for selecting a tag
         ExposedDropdownMenuBox(
             expanded = expanded,
-            onExpandedChange = { expanded = !expanded}
+            onExpandedChange = { expanded = !expanded }
         ) {
-            TextField(
+            OutlinedTextField(
                 value = selectedTag,
                 onValueChange = {},
                 readOnly = true,
                 label = { Text("Select a #Tag") },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .menuAnchor(),
-                trailingIcon = {
-                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
-                }
+                    .menuAnchor()
+                // trailingIcon = {
+                //    ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+                //}
             )
 
             ExposedDropdownMenu(
@@ -186,7 +204,7 @@ fun PostScreen(
             }
         }
 
-        Spacer(modifier = Modifier.height(12.dp))
+        Spacer(modifier = Modifier.height(16.dp))
 
         //Save & post Buttons
         /*Row(
@@ -211,35 +229,41 @@ fun PostScreen(
                     color = Color.Gray
                 )
             }*/
-            //Post Button
-            Button(
-                onClick = {
-                    if (title.isNotBlank() || content.isNotBlank() || imageUris.isNotEmpty()){
-                        showError = true
-                        return@Button
-                    }
-                    isPosting = true
-                    showError = false
+        //Post Button
+        Button(
+            onClick = {
+                if (title.isBlank() || content.isBlank() || imageUris.isEmpty()) {
+                    showError = true
+                    return@Button
+                }
+                isPosting = true
+                showError = false
 
-                    postViewModel.createPost(
-                        title = title,
-                        content = content,
-                        imageUris = imageUris,
-                        tags = listOf(selectedTag)
-                    ) { success ->
-                        isPosting = false
-                        if (success) {
-                            onBackClick()
-                        }
+                postViewModel.createPost(
+                    title = title,
+                    content = content,
+                    imageUris = imageUris,
+                    tags = listOf(selectedTag)
+                ) { success ->
+                    isPosting = false
+                    if (success) {
+                        onBackClick()
                     }
+                }
 
-                },
-                enabled = !isPosting, //Disable when posting
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
-                shape = RoundedCornerShape(8.dp),
-                modifier = Modifier.padding(8.dp)
-            ) {
+            },
+            enabled = !isPosting, //Disable when posting
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
+            shape = RoundedCornerShape(12.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+        ) {
+            if (isPosting) {
+                Text("Posting...", color = Color.Blue)
+            } else {
                 Text(text = stringResource(id = R.string.post), color = Color.White)
             }
         }
+    }
 }
