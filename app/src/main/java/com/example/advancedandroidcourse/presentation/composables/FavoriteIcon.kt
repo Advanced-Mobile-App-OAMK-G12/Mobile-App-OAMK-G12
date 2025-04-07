c77d30c3f46c233daa3315b40e53852aa1900855
XuanzhaoS <t3zhxu00@students.oamk.fi>
2025-04-07 14:28:27 +0300

    disable ripple effect on FavoriteIcon

--- app/src/main/java/com/example/advancedandroidcourse/presentation/composables/FavoriteIcon.kt
+++ app/src/main/java/com/example/advancedandroidcourse/presentation/composables/FavoriteIcon.kt
@@ -1,12 +1,18 @@
 package com.example.advancedandroidcourse.presentation.composables
 
+import android.text.Layout
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -19,15 +25,22 @@ fun FavoriteIcon(
     isSaved: Boolean,
     onToggleSaved: () -> Unit
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
 
-    IconButton(
-        onClick = onToggleSaved,
-        modifier = Modifier.size(24.dp)
+    Box(
+        modifier = modifier
+            .size(24.dp)
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null,
+                onClick = onToggleSaved
+            ),
+        contentAlignment = Alignment.Center
     ) {
         Icon(
-            painter = painterResource(id = if (isSaved) R.drawable.favorite_filled else R.drawable.favorite),
-            contentDescription = "Save",
-            tint = Color.Red
+                painter = painterResource(id = if (isSaved) R.drawable.favorite_filled else R.drawable.favorite),
+                contentDescription = "Save",
+                tint = Color.Red
         )
     }
 }
