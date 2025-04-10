703bfc0dbd00f8c6f2d620873e005964a37543c8
shyamharsh <t3wika00@students.oamk.fi>
2025-04-10 21:13:44 +0300

    Add the logo to SaveTipsScreen

--- app/src/main/java/com/example/advancedandroidcourse/presentation/savedTips/SaveTipsScreen.kt
+++ app/src/main/java/com/example/advancedandroidcourse/presentation/savedTips/SaveTipsScreen.kt
@@ -1,5 +1,6 @@
 package com.example.advancedandroidcourse.presentation.savedTips
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -21,11 +23,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.example.advancedandroidcourse.R
 import com.example.advancedandroidcourse.presentation.composables.BottomBar
 import com.example.advancedandroidcourse.presentation.composables.TipCard
 import com.example.advancedandroidcourse.presentation.notifications.NotificationViewModel
@@ -51,6 +55,14 @@ fun SaveTipsScreen(
                 top = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
             )
     ) {
+        //Adding Logo
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Easy Finn Logo",
+            modifier = Modifier
+                .padding(start = 16.dp, top = 16.dp)
+                .size(52.dp)
+        )
 
         //Heading
         Text(
